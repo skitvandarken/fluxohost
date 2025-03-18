f7a87da6e9431151d1c27e5f5cd33fc30bd4b137
Pedro Jorge <skitvandarken2013@gmail.com>
2025-03-18 19:32:10 +0100

    mvp botoes desactivados

--- src/app/paginas/hosting/hosting.component.ts
+++ src/app/paginas/hosting/hosting.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../layout/menu/menu.component';
+import { PrecosComponent } from '../../layout/precos/precos.component';
+import { RodapeComponent } from '../../layout/rodape/rodape.component';
 
 @Component({
   selector: 'app-hosting',
   imports: [
-    MenuComponent
+    MenuComponent,
+    PrecosComponent,
+    RodapeComponent
   ],
   templateUrl: './hosting.component.html',
   styleUrl: './hosting.component.css'
--- src/app/paginas/inicio/inicio.component.ts
+++ src/app/paginas/inicio/inicio.component.ts
@@ -3,15 +3,17 @@ import { MenuComponent } from '../../layout/menu/menu.component';
 import { CarrosselComponent } from '../../layout/carrossel/carrossel.component';
 import { ServicosComponent } from '../../layout/servicos/servicos.component';
 import { ContactoComponent } from '../../layout/contacto/contacto.component';
+import { RodapeComponent } from "../../layout/rodape/rodape.component";
 
 @Component({
   selector: 'app-inicio',
   imports: [
-   MenuComponent,
-   CarrosselComponent,
-   ServicosComponent,
-   ContactoComponent,
-  ],
+    MenuComponent,
+    CarrosselComponent,
+    ServicosComponent,
+    ContactoComponent,
+    RodapeComponent
+],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
