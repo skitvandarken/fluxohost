b25ff7877eb05cbd09a186e7f7743fc822803d0a
Pedro Jorge <skitvandarken2013@gmail.com>
2025-04-06 17:49:44 +0100

    mvp page vpn

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { DomainComponent } from './paginas/domain/domain.component';
 import { FaqComponentComponent } from './paginas/faq-component/faq-component.component';
 import { TermsComponent } from './terms/terms.component';
 import { PoliticasComponent } from './politicas/politicas.component';
+import { RedeComponent } from './paginas/rede/rede.component';
 
 export const routes: Routes = [
     {path:'', component:InicioComponent},
@@ -14,5 +15,6 @@ export const routes: Routes = [
     {path:'domain', component: DomainComponent},
     {path:'faq', component: FaqComponentComponent},
     {path:'termos-condicoes', component:TermsComponent},
-    {path: 'politicas', component:PoliticasComponent}
+    {path: 'politicas', component:PoliticasComponent},
+    {path:'noadsvpn', component:RedeComponent}
 ];
--- src/app/paginas/rede/rede.component.ts
+++ src/app/paginas/rede/rede.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../layout/menu/menu.component';
 import { RodapeComponent } from '../../layout/rodape/rodape.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-rede',
-  imports: [MenuComponent, RodapeComponent],
+  imports: [MenuComponent, RodapeComponent, RouterLink],
   templateUrl: './rede.component.html',
   styleUrl: './rede.component.css'
 })
